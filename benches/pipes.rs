de5b28764c1fb56eb30d11ad4fca9afb9ad42693
aleics <aleics.casanovas@gmail.com>
2020-11-06 21:17:20 +0100

    Adapt new source struct in benches

--- benches/pipes.rs
+++ benches/pipes.rs
@@ -3,137 +3,138 @@ extern crate test;
 
 use test::Bencher;
 
-use ebooler::vars::{Variable, Variables};
+use ebooler::data::DataItem;
 
+use transformer::data::DataValue;
 use transformer::filter::FilterPipe;
 use transformer::group::{GroupPipe, Operation};
 use transformer::map::MapPipe;
 use transformer::pipe::Pipe;
-use transformer::{run, Data};
+use transformer::{run, Source};
 
 #[bench]
 fn bench_filter_pipe_1(b: &mut Bencher) {
-  let values = vec![Variables::from_pairs(vec![("a", Variable::Number(1.0))])];
+  let data = vec![DataValue::from_pairs(vec![("a", DataItem::Number(1.0))])];
   let pipes = vec![Pipe::Filter(
     FilterPipe::new("(a > 1) && (a < 4) && (a != 3)").unwrap(),
   )];
 
-  let data = Data { values, pipes };
-  b.iter(|| run(&data).collect::<Vec<Variables>>());
+  let source = Source::new(data, pipes);
+  b.iter(|| run(&source).collect::<Vec<DataValue>>());
 }
 
 #[bench]
 fn bench_filter_pipe_20_sequentially(b: &mut Bencher) {
-  let values = vec![
-    Variables::from_pairs(vec![("a", Variable::Number(1.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(2.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(3.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(4.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(5.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(6.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(7.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(8.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(9.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(10.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(11.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(12.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(13.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(14.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(15.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(16.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(17.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(18.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(19.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(20.0))]),
+  let data = vec![
+    DataValue::from_pairs(vec![("a", DataItem::Number(1.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(2.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(3.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(4.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(5.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(6.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(7.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(8.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(9.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(10.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(11.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(12.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(13.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(14.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(15.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(16.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(17.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(18.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(19.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(20.0))]),
   ];
   let pipes = vec![Pipe::Filter(
     FilterPipe::new("(a > 1) && (a < 4) && (a != 3)").unwrap(),
   )];
 
-  let data = Data { values, pipes };
-  b.iter(|| run(&data).collect::<Vec<Variables>>());
+  let source = Source::new(data, pipes);
+  b.iter(|| run(&source).collect::<Vec<DataValue>>());
 }
 
 #[bench]
 fn bench_map_pipe_1(b: &mut Bencher) {
-  let values = vec![Variables::from_pairs(vec![("a", Variable::Number(1.0))])];
+  let data = vec![DataValue::from_pairs(vec![("a", DataItem::Number(1.0))])];
   let pipes = vec![Pipe::Map(
     MapPipe::new("(a + 1) / (a * 4) - (a + 2)", "b").unwrap(),
   )];
 
-  let data = Data { values, pipes };
-  b.iter(|| run(&data).collect::<Vec<Variables>>());
+  let source = Source::new(data, pipes);
+  b.iter(|| run(&source).collect::<Vec<DataValue>>());
 }
 
 #[bench]
 fn bench_map_pipe_20_sequentially(b: &mut Bencher) {
-  let values = vec![
-    Variables::from_pairs(vec![("a", Variable::Number(1.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(2.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(3.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(4.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(5.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(6.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(7.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(8.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(9.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(10.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(11.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(12.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(13.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(14.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(15.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(16.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(17.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(18.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(19.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(20.0))]),
+  let data = vec![
+    DataValue::from_pairs(vec![("a", DataItem::Number(1.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(2.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(3.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(4.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(5.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(6.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(7.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(8.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(9.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(10.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(11.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(12.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(13.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(14.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(15.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(16.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(17.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(18.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(19.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(20.0))]),
   ];
   let pipes = vec![Pipe::Map(
     MapPipe::new("(a + 1) / (a * 4) - (a + 2)", "b").unwrap(),
   )];
 
-  let data = Data { values, pipes };
-  b.iter(|| run(&data).collect::<Vec<Variables>>());
+  let source = Source::new(data, pipes);
+  b.iter(|| run(&source).collect::<Vec<DataValue>>());
 }
 
 #[bench]
 fn bench_group_pipe_1(b: &mut Bencher) {
-  let values = vec![Variables::from_pairs(vec![("a", Variable::Number(1.0))])];
+  let data = vec![DataValue::from_pairs(vec![("a", DataItem::Number(1.0))])];
   let pipes = vec![Pipe::Group(GroupPipe::new("a", Operation::Count, "count"))];
 
-  let data = Data { values, pipes };
-  b.iter(|| run(&data).collect::<Vec<Variables>>());
+  let source = Source::new(data, pipes);
+  b.iter(|| run(&source).collect::<Vec<DataValue>>());
 }
 
 #[bench]
 fn bench_group_pipe_20_sequentially(b: &mut Bencher) {
-  let values = vec![
-    Variables::from_pairs(vec![("a", Variable::Number(1.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(2.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(3.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(4.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(5.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(6.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(7.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(8.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(9.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(10.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(11.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(12.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(13.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(14.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(15.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(16.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(17.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(18.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(19.0))]),
-    Variables::from_pairs(vec![("a", Variable::Number(20.0))]),
+  let data = vec![
+    DataValue::from_pairs(vec![("a", DataItem::Number(1.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(2.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(3.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(4.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(5.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(6.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(7.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(8.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(9.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(10.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(11.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(12.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(13.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(14.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(15.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(16.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(17.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(18.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(19.0))]),
+    DataValue::from_pairs(vec![("a", DataItem::Number(20.0))]),
   ];
   let pipes = vec![Pipe::Group(GroupPipe::new("a", Operation::Count, "count"))];
 
-  let data = Data { values, pipes };
-  b.iter(|| run(&data).collect::<Vec<Variables>>());
+  let source = Source::new(data, pipes);
+  b.iter(|| run(&source).collect::<Vec<DataValue>>());
 }
 
 #[bench]
@@ -151,33 +152,33 @@ fn bench_10_pipes_2_10_vars_maps(b: &mut Bencher) {
     Pipe::Map(MapPipe::new("(j + 10)", "t").unwrap()),
   ];
 
-  let values = vec![
-    Variables::from_pairs(vec![
-      ("a", Variable::Number(1.0)),
-      ("b", Variable::Number(2.0)),
-      ("c", Variable::Number(3.0)),
-      ("d", Variable::Number(4.0)),
-      ("e", Variable::Number(5.0)),
-      ("f", Variable::Number(6.0)),
-      ("g", Variable::Number(7.0)),
-      ("h", Variable::Number(8.0)),
-      ("i", Variable::Number(9.0)),
-      ("j", Variable::Number(10.0)),
+  let data = vec![
+    DataValue::from_pairs(vec![
+      ("a", DataItem::Number(1.0)),
+      ("b", DataItem::Number(2.0)),
+      ("c", DataItem::Number(3.0)),
+      ("d", DataItem::Number(4.0)),
+      ("e", DataItem::Number(5.0)),
+      ("f", DataItem::Number(6.0)),
+      ("g", DataItem::Number(7.0)),
+      ("h", DataItem::Number(8.0)),
+      ("i", DataItem::Number(9.0)),
+      ("j", DataItem::Number(10.0)),
     ]),
-    Variables::from_pairs(vec![
-      ("a", Variable::Number(1.0)),
-      ("b", Variable::Number(2.0)),
-      ("c", Variable::Number(3.0)),
-      ("d", Variable::Number(4.0)),
-      ("e", Variable::Number(5.0)),
-      ("f", Variable::Number(6.0)),
-      ("g", Variable::Number(7.0)),
-      ("h", Variable::Number(8.0)),
-      ("i", Variable::Number(9.0)),
-      ("j", Variable::Number(10.0)),
+    DataValue::from_pairs(vec![
+      ("a", DataItem::Number(1.0)),
+      ("b", DataItem::Number(2.0)),
+      ("c", DataItem::Number(3.0)),
+      ("d", DataItem::Number(4.0)),
+      ("e", DataItem::Number(5.0)),
+      ("f", DataItem::Number(6.0)),
+      ("g", DataItem::Number(7.0)),
+      ("h", DataItem::Number(8.0)),
+      ("i", DataItem::Number(9.0)),
+      ("j", DataItem::Number(10.0)),
     ]),
   ];
 
-  let data = Data { values, pipes };
-  b.iter(|| run(&data).collect::<Vec<Variables>>());
+  let source = Source::new(data, pipes);
+  b.iter(|| run(&source).collect::<Vec<DataValue>>());
 }
