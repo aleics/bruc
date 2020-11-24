ebddd60f9e70386c3e3e61243473a3cdeceeb065
aleics <aleics.casanovas@gmail.com>
2020-11-24 08:26:56 +0100

    Add scales to specification

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use crate::scale::Scale;
 use crate::transform::Transform;
 
 pub mod data;
@@ -21,17 +22,22 @@ impl<'a> Engine<'a> {
 pub struct Specification<'a> {
   #[cfg_attr(feature = "serde", serde(borrow))]
   transform: Option<Transform<'a>>,
+  scales: Vec<Scale<'a>>,
 }
 
 impl<'a> Specification<'a> {
-  pub fn new(transform: Option<Transform<'a>>) -> Specification<'a> {
-    Specification { transform }
+  pub fn new(transform: Option<Transform<'a>>, scales: Vec<Scale<'a>>) -> Specification<'a> {
+    Specification { transform, scales }
   }
 }
 
 #[cfg(feature = "serde")]
 #[cfg(test)]
 mod serde_tests {
+  use crate::scale::domain::Domain;
+  use crate::scale::linear::LinearScale;
+  use crate::scale::range::Range;
+  use crate::scale::Scale;
   use crate::transform::filter::FilterPipe;
   use crate::transform::pipe::Pipe;
   use crate::transform::Transform;
@@ -39,8 +45,30 @@ mod serde_tests {
 
   #[test]
   fn deserializes_empty_spec() {
-    let spec: Specification = serde_json::from_str("{}").unwrap();
-    assert_eq!(spec, Specification::new(None));
+    let spec: Specification = serde_json::from_str(
+      r#"{
+        "scales": [ 
+          {
+            "type": "linear",
+            "name": "horizontal",
+            "domain": [0, 100],
+            "range": [0, 20]
+          }
+        ]
+      }"#,
+    )
+    .unwrap();
+    assert_eq!(
+      spec,
+      Specification::new(
+        None,
+        vec![Scale::Linear(LinearScale::new(
+          "horizontal",
+          Domain::Literal(0.0, 100.0),
+          Range::Literal(0.0, 20.0),
+        ))],
+      )
+    );
   }
 
   #[test]
@@ -52,16 +80,31 @@ mod serde_tests {
           "pipes": [
             { "type": "filter", "fn": "a > 2" }
           ]
-        }
+        },
+        "scales": [ 
+          {
+            "type": "linear",
+            "name": "horizontal",
+            "domain": [0, 100],
+            "range": [0, 20]
+          }
+        ]
       }"#,
     )
     .unwrap();
     assert_eq!(
       spec,
-      Specification::new(Some(Transform::new(
-        "primary",
-        vec![Pipe::Filter(FilterPipe::new("a > 2").unwrap())],
-      )))
+      Specification::new(
+        Some(Transform::new(
+          "primary",
+          vec![Pipe::Filter(FilterPipe::new("a > 2").unwrap())],
+        )),
+        vec![Scale::Linear(LinearScale::new(
+          "horizontal",
+          Domain::Literal(0.0, 100.0),
+          Range::Literal(0.0, 20.0),
+        ))],
+      )
     );
   }
 }
