ede2672296d88d82ff9c71d9f5496ddcf8208d09
aleics <aleics.casanovas@gmail.com>
2024-08-30 17:17:14 +0200

    Add pie wasm example

---
+++ bruc-web/examples/pie-chart/index.js
@@ -0,0 +1,55 @@
+import { Bruc } from "bruc";
+
+const spec = `{
+  "dimensions": {
+    "width": 500,
+    "height": 500
+  },
+  "data": [
+    {
+      "name": "primary",
+      "values": [],
+      "transform": [
+        { "type": "map", "fn": "y * 10", "output": "value" }
+      ]
+    }
+  ],
+  "visual": {
+    "shapes": [
+      {
+        "from": "primary",
+        "type": "pie",
+        "properties": {
+          "value": { "field": "value" }
+        }
+      }
+    ]
+  }
+}`;
+
+const bruc = Bruc.build(spec);
+
+await bruc.renderAsSvg("#first");
+
+while (true) {
+  const data = randomData();
+  await bruc.setData("primary", data);
+
+  await delay(1000);
+}
+
+function randomData() {
+  const values = [];
+  for (let i = 0; i <= randomValue(40); i++) {
+    values.push({ x: i, y: randomValue(50) });
+  }
+  return values;
+}
+
+function randomValue(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function delay(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
